5030be712feb62cf8562ceaa0dadb5bfbf4707d2
SanderO7 <layatchrysander5@gmail.com>
2025-10-19 13:59:52 +0800

    UPDATE: Login.php & signup.php

--- php/login.php
+++ php/login.php
@@ -3,9 +3,42 @@ header('Content-Type: application/json');
 session_start();
 
 // Include database config
-require_once 'config.php';
+include 'config.php';
 
+$email = $_POST['email'] ?? '';
+$password = $_POST['password'] ?? '';
 
+if (empty($email) || empty($password)) {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Username and password are required.'
+    ]);
+    exit;
+}
 
+$stmt = $conn->prepare("SELECT id, password_hash FROM users WHERE username = ?");
+$stmt->bind_param("s", $email);   
+$stmt->execute();
+$result = $stmt->get_result();
 
+if($result->num_rows > 0){
+    $user = $result -> fetch_assoc();
+    if(password_verify($password, $user('password'))){
+        echo json_encode([
+            'success' => true,
+            'message' => 'Login successful',
+            'user' => [
+                'name' => $user['name'],
+                'role' => $user['userType']
+            ]
+        ]);
+    } else {
+        echo json_encode(['success' => false, 'message' => 'Incorrect Password']);
+    }
+} else {
+    echo json_encode('success' => false, 'message' => 'User not found');
+}
+
+$stmt->close();
+$conn->close();
 ?>
--- php/signup.php
+++ php/signup.php
@@ -5,11 +5,31 @@ session_start();
 // Include database config
 require_once 'config.php';
 
-$action = isset($_POST['action']) ? $_POST['action'] : '';
+$action = $_POST['action'] ?? '';
 
 //here in the action value, the javscript sent are 'register', 'verify', 'resend'
 //register - (name, email, password, userType). Gawa muna sya verification code then resend to the client
 // then the client will verify that and the script will send the 'verify' action.
+
+switch($action){
+    case 'register':
+        registerUser($conn);
+        break;
+    
+    case 'verify':
+        verifyUser($conn);
+        break;
+
+    case 'resend';
+        resendCode($conn);
+        break;
+
+    default:
+        echo json_encode(['success' => false, 'message' => 'Invalid action']);
+        break;
+}
+
+$conn->close();
 
 // verify - (email, code)
 
