5bb64a6c477f8631e15f1fd0038c4f266d64d4f9
zyrus <2240045@slu.edu.ph>
2025-11-17 21:59:36 +0800

    database

--- sql-database/karagacharge.sql
+++ sql-database/karagacharge.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 16, 2025 at 05:16 PM
+-- Generation Time: Nov 17, 2025 at 01:11 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -40,7 +40,17 @@ CREATE TABLE IF NOT EXISTS `booking` (
   PRIMARY KEY (`book_id`),
   KEY `fk_evown_id` (`evown_id`),
   KEY `fk_stat_id` (`stat_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`book_id`, `time_in`, `time_out`, `date`, `evown_id`, `rate`, `status`, `stat_id`) VALUES
+(1, 1000, 1100, '2025-10-08', 345671, 20, 'Completed', 3),
+(2, 900, 1000, '2025-11-18', 345671, 18.5, 'Completed', 1),
+(3, 1030, 1200, '2025-11-18', 735183, 12, 'Completed', 2),
+(4, 1300, 1430, '2025-11-18', 5047020, 20, 'Pending', 3);
 
 -- --------------------------------------------------------
 
@@ -67,6 +77,7 @@ CREATE TABLE IF NOT EXISTS `charging_provider` (
 
 INSERT INTO `charging_provider` (`id`, `email`, `password_hash`, `phoneno`, `name`, `verification_code`, `is_verified`) VALUES
 (2564089, 'markangelodomalanta05@gmail.com', '$2y$12$5uafh5mxOC/UpamlAHv76OXZ72fKpjCxqG99rF8koSv/lTi/3fV/6', '+639279126180', 'MARK ANGELO DOMALANTA', 429055, 1),
+(7736903, 'fleargamata@gmail.com', '$2y$12$5uafh5mxOC/UpamlAHv76OXZ72fKpjCxqG99rF8koSv/lTi/3fV/6', '+639279126180', 'Flear', 453574, 1),
 (9168225, 'layatchrysander5@gmail.com', '$2y$12$5uafh5mxOC/UpamlAHv76OXZ72fKpjCxqG99rF8koSv/lTi/3fV/6', '+639123456789', 'CHRYSANDER LAY-AT', 365012, 1);
 
 -- --------------------------------------------------------
@@ -88,7 +99,16 @@ CREATE TABLE IF NOT EXISTS `charging_station` (
   `stat_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`stat_id`),
   KEY `fk_provider` (`prov_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `charging_station`
+--
+
+INSERT INTO `charging_station` (`stat_id`, `location`, `place_type`, `charge_type`, `rate`, `availability_status`, `details`, `prov_id`, `stat_name`) VALUES
+(1, 'Bakakeng, Bareng Drive', 'Homeowner', 'Outlet', 18.5, 'Available', 'Beside Carwash', 2564089, 'Mark EV Station'),
+(2, 'Baguio, Kisad rd', 'Business Est.', 'Charging station', 12, 'Available', 'Coffee Shop & EV Chrging Station, Beside Crown Legacy', 7736903, 'Coffe&Charge Hub'),
+(3, 'Camp7,Baguio Benguet, Bonifacio St.', 'Homeowner', 'Charging station', 20, 'Occupied', 'Front of Soleo Inn, Beside Total gas station', 9168225, 'The Charger Bay');
 
 -- --------------------------------------------------------
 
@@ -114,7 +134,9 @@ CREATE TABLE IF NOT EXISTS `ev_owner` (
 --
 
 INSERT INTO `ev_owner` (`id`, `email`, `password_hash`, `phoneno`, `name`, `verification_code`, `is_verified`) VALUES
-(5047020, 'domalantamarkangelo@gmail.com', '$2y$12$faOtpxZZ5qKksbxZjJyiXOMBQY5qXhpWJxQgsJf3JanX8DH2zfehy', '+639279126180', 'MARK ANGELO DOMALANTA', 902295, 1);
+(345671, 'zyrusmasaad@gmail.com', '$2y$12$5uafh5mxOC/UpamlAHv76OXZ72fKpjCxqG99rF8koSv/lTi/3fV/6', '09876543218', 'Zyrus Masa-ad', 345683, 1),
+(735183, 'zysam2005@gmail.com', '$2y$12$5uafh5mxOC/UpamlAHv76OXZ72fKpjCxqG99rF8koSv/lTi/3fV/6', '0954628173', 'Zy', 827451, 1),
+(5047020, 'domalantamarkangelo@gmail.com', '$2y$12$5uafh5mxOC/UpamlAHv76OXZ72fKpjCxqG99rF8koSv/lTi/3fV/6', '+639279126180', 'MARK ANGELO DOMALANTA', 902295, 1);
 
 -- --------------------------------------------------------
 
@@ -131,7 +153,17 @@ CREATE TABLE IF NOT EXISTS `payment` (
   `book_id` int NOT NULL,
   PRIMARY KEY (`pay_id`),
   KEY `fk_book_id` (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`pay_id`, `total_amount`, `payment_method`, `payment_status`, `book_id`) VALUES
+(2, 185, 'GCash', 'Paid', 2),
+(3, 240, 'Cash', 'Paid', 3),
+(4, 300, 'GCash', 'Pending', 4),
+(5, 200, 'GCash', 'Paid', 1);
 
 -- --------------------------------------------------------
 
@@ -147,7 +179,16 @@ CREATE TABLE IF NOT EXISTS `review` (
   `pay_id` int NOT NULL,
   PRIMARY KEY (`review_id`),
   KEY `fk_pay_id` (`pay_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `review`
+--
+
+INSERT INTO `review` (`review_id`, `rating`, `comment`, `pay_id`) VALUES
+(1, 5, 'Fast and convenient charging!', 2),
+(2, 4, 'Good location but could add more charging bays.', 3),
+(3, 5, 'Very helpful provider!', 4);
 
 --
 -- Constraints for dumped tables
